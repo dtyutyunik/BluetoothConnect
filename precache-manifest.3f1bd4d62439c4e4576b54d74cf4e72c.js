830831e0edb1784e1f5b4d473d3a1e353fdf46a7
Dmitriy Tyutyunik <dmitriy.tyutyunik@gmail.com>
2020-01-09 14:47:16 -0500

    Updates

---
+++ precache-manifest.3f1bd4d62439c4e4576b54d74cf4e72c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9254349dd6783daab0caec5bce447d2",
+    "url": "/BluetoothConnect/index.html"
+  },
+  {
+    "revision": "c00e37a19dd6752bed87",
+    "url": "/BluetoothConnect/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e5baec226c269d1ee9fb",
+    "url": "/BluetoothConnect/static/js/2.44ec59e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/BluetoothConnect/static/js/2.44ec59e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c00e37a19dd6752bed87",
+    "url": "/BluetoothConnect/static/js/main.46d48c8e.chunk.js"
+  },
+  {
+    "revision": "197af568059c3b094690",
+    "url": "/BluetoothConnect/static/js/runtime-main.e6fb6548.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BluetoothConnect/static/media/logo.5d5d9eef.svg"
+  }
+]);
